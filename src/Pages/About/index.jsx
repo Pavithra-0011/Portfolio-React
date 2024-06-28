4734ad7f8e09b2115dc55b7d500a9ad82f5d5dbd
Pavithra-0011 <pavithraanshaya@gmail.com>
2024-06-28 20:59:16 +0530

    Image-path

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -79,33 +79,33 @@ function About() {
            <h2 className='text-center text-4xl'>Technical Skills</h2>
            <div className='flex flex-row justify-between w-90 px-3 py-2 mx-5 mt-20 font-semibold text-center'>
             <div className='flex flex-col w-fit px-2'>
-            <img src='src/assets/html.png' className='m-auto' alt='html-img' height='40px' width='40px'/>
+            <img src='public/assets/html.png' className='m-auto' alt='html-img' height='40px' width='40px'/>
              <h4 className='Tech-text mt-3'>HTML</h4>
              
             </div>
             <div className='flex flex-col'>
-            <img src='src/assets/css-3.png' alt='html-img' className='m-auto' height='40px' width='40px'/>
+            <img src='public/assets/css-3.png' alt='html-img' className='m-auto' height='40px' width='40px'/>
              <h4 className='Tech-text mt-3'>CSS</h4>       
             </div>
             <div className='flex flex-col'>
-            <img src='src/assets/java-script.png' className='m-auto' alt='html-img' height='40px' width='40px'/>
+            <img src='public/assets/java-script.png' className='m-auto' alt='html-img' height='40px' width='40px'/>
              <h4 className='Tech-text mt-3'>JS</h4>             
             </div>
             <div className='flex flex-col'>
-            <img src='src/assets/bootstrap.png' className='m-auto' alt='html-img' height='40px' width='40px'/>
+            <img src='public/assets/bootstrap.png' className='m-auto' alt='html-img' height='40px' width='40px'/>
              <h4 className='Tech-text mt-3'>BS</h4>             
             </div>
             <div className='flex flex-col'>
-            <img src='src/assets/wind.png' className='m-auto' alt='html-img' height='40px' width='40px'/>
+            <img src='public/assets/wind.png' className='m-auto' alt='html-img' height='40px' width='40px'/>
              <h4 className='Tech-text mt-3'>TW</h4>
             </div>
             <div className='flex flex-col'>
-            <img src='src/assets/atom.png' className='m-auto' alt='html-img' height='40px' width='40px'/>
+            <img src='public/assets/atom.png' className='m-auto' alt='html-img' height='40px' width='40px'/>
              <h4 className='Tech-text mt-3'>REACT</h4>
              
             </div>
             <div className='flex flex-col w-fit px-2'>
-             <img src='src/assets/angularjs.svg' className='m-auto' alt='html-img' height='40px' width='40px'/>
+             <img src='public/assets/angularjs.svg' className='m-auto' alt='html-img' height='40px' width='40px'/>
              <h4 className='Tech-text mt-3'>ANGULAR</h4>
             </div>
            </div>
@@ -124,12 +124,12 @@ function About() {
            <h1>Hands on Experience on Real Time Projects</h1>
            <div className='flex mt-14 justify-evenly'>
            <div className='flex flex-col'>
-            <img src='src/assets/haircut.png' className='m-auto' alt='html-img' height='40px' width='40px'/>
+            <img src='public/assets/haircut.png' className='m-auto' alt='html-img' height='40px' width='40px'/>
              <h4 className='mt-3 text-xl'>TONI INTERNATIONAL</h4>
              <p className='text-sm'>(A React based project)</p>
            </div>
            <div className='flex flex-col'>
-            <img src='src/assets/music.png' className='m-auto' alt='html-img' height='80px' width='80px'/>
+            <img src='public/assets/music.png' className='m-auto' alt='html-img' height='80px' width='80px'/>
              <h4 className='text-xl mt-3'>ICON</h4>
              <p className='text-sm'>(An Angular based project)</p>
             </div>
@@ -149,7 +149,7 @@ function About() {
              <h1 className='text-center'>Certifications</h1>
           <div className='flex justify-between w-10/12'>
           <div>
-           <img src='src/assets/document.png' alt='Contact' height="180px" width="150px"/>
+           <img src='public/assets/document.png' alt='Contact' height="180px" width="150px"/>
            </div>
            <div className='mt-10 leading-5'>
              <p className='text-white text-2xl'>1.Data Science for Engineers (NPTEL - IIT Madras).</p>
@@ -182,15 +182,15 @@ function About() {
             <div className='Contact'>
              <div className='flex justify-evenly text-white mt-10'>
               <a href='https://www.linkedin.com/in/pavithra-s-a67ba3247' target='blank'>
-               <img src='src/assets/linkedin.png' alt='html-img' height='30px' width='30px'/>
+               <img src='public/assets/linkedin.png' alt='html-img' height='30px' width='30px'/>
               </a>
               <a href='https://www.instagram.com/__miss__virgo?igsh=MWJteHB6aWtyeWRwNA==' target='blank'>
-               <img src='src/assets/instagram.png' alt='html-img' height='30px' width='30px'/>
+               <img src='public/assets/instagram.png' alt='html-img' height='30px' width='30px'/>
               </a>
               <a href='https://www.facebook.com/profile.php?id=100017325847000' target='blank'>
-               <img src='src/assets/facebook.png' alt='html-img' height='30px' width='30px'/>
+               <img src='public/assets/facebook.png' alt='html-img' height='30px' width='30px'/>
               </a>
-               <img src='src/assets/gmail.png' alt='html-img' height='30px' width='30px'/>
+               <img src='public/assets/gmail.png' alt='html-img' height='30px' width='30px'/>
              </div>
             </div>
          </motion.div>
